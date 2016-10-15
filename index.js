a1d4542f52c82578af1f3c29f3007f2faa19ce71
jefflete <jeffreywu@msusd.org>
2016-10-15 10:36:33 -0700

    more npm

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+// app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  // res.render('index')
+})
+
+app.listen(process.env.PORT || 2000, function () {
+  console.log('Express server started on port ' + (process.env.PORT || 2000))
+})
